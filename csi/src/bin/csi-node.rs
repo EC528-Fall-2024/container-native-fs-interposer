a12e97ba6a92bd649d9bdf26dec52e782377824e
Nick Cao <nickcao@nichi.co>
2024-09-23 17:15:03 -0400

    Implement CSI node plugin entrypoint

---
+++ csi/src/bin/csi-node.rs
@@ -0,0 +1,26 @@
+use container_native_fs_interposer::{
+    csi::v1::{identity_server::IdentityServer, node_server::NodeServer},
+    identity::IdentityService,
+    node::NodeService,
+};
+use std::{env, io::ErrorKind};
+use tokio::net::UnixListener;
+use tokio_stream::wrappers::UnixListenerStream;
+use tonic::transport::Server;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = env::var("CSI_ENDPOINT")?;
+    match std::fs::remove_file(&path) {
+        Err(err) if err.kind() == ErrorKind::NotFound => (),
+        result => result?,
+    }
+    Server::builder()
+        .add_service(IdentityServer::new(IdentityService {}))
+        .add_service(NodeServer::new(NodeService::new(&env::var(
+            "KUBE_NODE_NAME",
+        )?)))
+        .serve_with_incoming(UnixListenerStream::new(UnixListener::bind(&path)?))
+        .await?;
+    Ok(())
+}
