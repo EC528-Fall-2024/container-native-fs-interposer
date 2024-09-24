823a8064f7d1dabc7b77af50d9b399ec5f64148c
CleanerC <xjw031201@gmail.com>
2024-09-24 07:35:33 -0400

    finished the initial loop setup

--- FUSE1/FUSE_ll.cpp
+++ FUSE1/FUSE_ll.cpp
@@ -35,6 +35,11 @@ enum {
     CACHE_ALWAYS,
 };
 
+
+/* 
+ *  following two data structures are not needed for now
+ *  to learn more go to example code provided by libfuse
+ * */
 struct lo_inode {
     struct lo_inode* next;
     struct lo_inode* prev;
@@ -44,7 +49,7 @@ struct lo_inode {
     uint64_t refcount;
 };
 
-struct lo_data {
+struct lo_data {            
     pthread_mutex_t mutex;
     int debug;
     int writeback;
@@ -57,25 +62,28 @@ struct lo_data {
     struct lo_inode root;   /* protected my lo->mutex */
 };
 
-static const struct fuse_lowlevel_ops operations = { };     //tobe implemented
+
+/*
+ *  this is where all the function this fuse going to support
+ * */
+static const struct fuse_lowlevel_ops operations = {
+        
+};     //tobe implemented
 
 int main(int argc, char *argv[])
 {
     struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
     struct fuse_session *se;      
     struct fuse_cmdline_opts opts;
-    struct fuse_loop_config config;
-    struct lo_data lo = { .debug = 0,
-                          .writeback = 0};
+    #struct fuse_loop_config config;
+    #struct lo_data lo;
+    int ret = -1;
     
-    int ret = -1;       //return code
-
-    umask(0);
-
-    pthread_mutex_init(&lo.mutex, NULL);         //not gonna support multiple write for now
-    lo.root.next = lo.root.prev = &lo.root;
-    lo.root.fd = -1;
-    lo.cache = CACHE_NORMAL;
+    // initializing lo_data
+    #pthread_mutex_init(&lo.mutex, NULL);         //not gonna support multiple write for now
+    #lo.root.next = lo.root.prev = &lo.root;
+    #lo.root.fd = -1;
+    #lo.cache = CACHE_NORMAL;
 
     if(fuse_parse_cmdline(&args, &opts) != 0)
         return -1;
@@ -85,19 +93,19 @@ int main(int argc, char *argv[])
         fuse_cmdline_help();
         fuse_lowlevel_help();
         ret = 0;
-        goto err_out1;
+        return ret;
     } else if (opts.show_version) {
         printf("FUSE library version %s\n", fuse_pkgversion());
         fuse_lowlevel_version();
         ret = 0;
-        goto err_out1;
+        return ret;    
     }
 
     if(opts.mountpoint == NULL) {
         printf("Usage: %s [options] <mountpoint>\n\n", argv[0]);
         printf("       %s --help\n", argv[0]);
-        ret = 1;
-        goto err_out1;
+        ret = 0;
+        return ret;
     }
     
     printf("STEP 1: opts.mountpoint is not NULL : %s\n", opts.mountpoint);
@@ -106,19 +114,40 @@ int main(int argc, char *argv[])
     if( se == NULL ) {
         printf("fuse_session_new() failed \n");
         ret = -1;
-        return -1;
+        return ret;
     }
     
     printf("STEP 2: fuse_session_new() success, fuse_session (%p) \n", se);
+    if( fuse_set_signal_handlers(se) != 0 ) {
+        printf("failed to setup signal_handler for se \n");
+        ret = -1;
+        return ret;
+    }
+    
+    printf("STEP 3: Setup signal handler OK \n");
+    
+    if( fuse_session_mount(se, opts.mountpoint) != 0 ) {
+        printf("se failed to mount \n");
+        ret = -1;
+        return ret;
+    }
 
+    printf("STEP 4: fuse_ssession_mount() success \n");
+    
+    fuse_daemonize(opts.foreground);           //for testing purpose, want to see cmdline output
+    
+    ret = fuse_session_loop(se);               //block untill ctl+c or fusemount -u (unmount). entering loop
+    
+    fuse_session_unmount(se);
+    printf("STEP 5: fuse_session_unmount() \n");
 
-err_out1:
-    free(opts.mountpoint);
+    fuse_remove_signal_handlers(se);
+    printf("STEP 6: fuse_remove_signal_handlers(se) \n");
+
+    fuse_session_destroy(se);
+    printf("STEP 7: fuse_session_destroy(se) \n");
+
     fuse_opt_free_args(&args);
-
-    if(lo.root.fd >= 0)
-        close(lo.root.fd);
-
-    free(lo.source);
-    return ret ? 1 : 0; 
+    ret = 0;
+    return ret;
 }
