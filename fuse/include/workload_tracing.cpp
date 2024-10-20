381bec08fc491500888c482a2137827a879a2fe4
Julia <jhua2@bu.edu>
2024-10-20 10:01:40 -0400

    Add attributes

--- fuse/include/workload_tracing.cpp
+++ fuse/include/workload_tracing.cpp
@@ -58,6 +58,8 @@ static void tracing_destroy(void *userdata) {
 
 static void tracing_lookup(fuse_req_t req, fuse_ino_t parent, const char *name) {
 	auto span = getSpan(LIB_NAME, "Lookup");
+	setAttribute(span, req);
+	span->SetAttribute("Name", name);
 	tracing_next->lookup(req, parent, name);
 	span->End();
 }
@@ -65,6 +67,8 @@ static void tracing_lookup(fuse_req_t req, fuse_ino_t parent, const char *name) 
 static void tracing_mkdir(fuse_req_t req, fuse_ino_t parent, const char *name,
     mode_t mode) {
 	auto span = getSpan(LIB_NAME, "Mkdir");
+	setAttribute(span, req);
+	span->SetAttribute("Name", name);
 	tracing_next->mkdir(req, parent, name, mode);
 	span->End();
 }
@@ -72,6 +76,8 @@ static void tracing_mkdir(fuse_req_t req, fuse_ino_t parent, const char *name,
 static void tracing_mknod(fuse_req_t req, fuse_ino_t parent, const char *name,
 	mode_t mode, dev_t rdev) {
 	auto span = getSpan(LIB_NAME, "Mknod");
+	setAttribute(span, req);
+	span->SetAttribute("Name", name);
 	tracing_next->mknod(req, parent, name, mode, rdev);
 	span->End();
 }
@@ -79,6 +85,8 @@ static void tracing_mknod(fuse_req_t req, fuse_ino_t parent, const char *name,
 static void tracing_symlink(fuse_req_t req, const char *link, fuse_ino_t parent,
 	const char *name) {
 	auto span = getSpan(LIB_NAME, "Symlink");
+	setAttribute(span, req);
+	span->SetAttribute("Name", name);
 	tracing_next->symlink(req, link, parent, name);
 	span->End();
 }
@@ -86,18 +94,25 @@ static void tracing_symlink(fuse_req_t req, const char *link, fuse_ino_t parent,
 static void tracing_link(fuse_req_t req, fuse_ino_t ino, fuse_ino_t parent,
 	const char *name) {
 	auto span = getSpan(LIB_NAME, "Link");
+	setAttribute(span, req);
+	setAttribute(span, ino);
+	span->SetAttribute("Name", name);
 	tracing_next->link(req, ino, parent, name);
 	span->End();
 }
 
 static void tracing_unlink(fuse_req_t req, fuse_ino_t parent, const char *name) {
 	auto span = getSpan(LIB_NAME, "Unlink");
+	setAttribute(span, req);
+	span->SetAttribute("Name", name);
 	tracing_next->unlink(req, parent, name);
 	span->End();
 }
 
 static void tracing_rmdir(fuse_req_t req, fuse_ino_t parent, const char *name) {
 	auto span = getSpan(LIB_NAME, "Rmdir");
+	setAttribute(span, req);
+	span->SetAttribute("Name", name);
 	tracing_next->rmdir(req, parent, name);
 	span->End();
 }
@@ -105,12 +120,16 @@ static void tracing_rmdir(fuse_req_t req, fuse_ino_t parent, const char *name) {
 static void tracing_rename(fuse_req_t req, fuse_ino_t parent, const char *name,
 	fuse_ino_t newparent, const char *newname, unsigned int flags) {
 	auto span = getSpan(LIB_NAME, "Rename");
+	setAttribute(span, req);
+	span->SetAttribute("Name", name);
 	tracing_next->rename(req, parent, name, newparent, newname, flags);
 	span->End();
 }
 
 static void tracing_forget(fuse_req_t req, fuse_ino_t ino, uint64_t nlookup) {
 	auto span = getSpan(LIB_NAME, "Forget");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->forget(req, ino, nlookup);
 	span->End();
 }
@@ -118,12 +137,15 @@ static void tracing_forget(fuse_req_t req, fuse_ino_t ino, uint64_t nlookup) {
 static void tracing_forget_multi(fuse_req_t req, size_t count,
 	fuse_forget_data *forgets) { 
 	auto span = getSpan(LIB_NAME, "Forget Multi");
+	setAttribute(span, req);
 	tracing_next->forget_multi(req, count, forgets);
 	span->End();
 }
 
 static void tracing_getattr(fuse_req_t req, fuse_ino_t ino, fuse_file_info *fi) {
 	auto span = getSpan(LIB_NAME, "Get Attribute");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->getattr(req, ino, fi);
 	span->End();
 }
@@ -131,18 +153,24 @@ static void tracing_getattr(fuse_req_t req, fuse_ino_t ino, fuse_file_info *fi) 
 static void tracing_setattr(fuse_req_t req, fuse_ino_t ino, struct stat *attr,
 	int valid, fuse_file_info *fi) {
 	auto span = getSpan(LIB_NAME, "Set Attribute");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->setattr(req, ino, attr, valid, fi);
 	span->End();
 }
 
 static void tracing_readlink(fuse_req_t req, fuse_ino_t ino) {
 	auto span = getSpan(LIB_NAME, "Read Link");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->readlink(req, ino);
 	span->End();
 }
 
 static void tracing_opendir(fuse_req_t req, fuse_ino_t ino, fuse_file_info *fi) {
 	auto span = getSpan(LIB_NAME, "Open Directory");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->opendir(req, ino, fi);
 	span->End();
 }
@@ -150,6 +178,8 @@ static void tracing_opendir(fuse_req_t req, fuse_ino_t ino, fuse_file_info *fi) 
 static void tracing_readdir(fuse_req_t req, fuse_ino_t ino, size_t size,
 	off_t offset, fuse_file_info *fi) {
 	auto span = getSpan(LIB_NAME, "Read Directory");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->readdir(req, ino, size, offset, fi);
 	span->End();
 }
@@ -157,12 +187,16 @@ static void tracing_readdir(fuse_req_t req, fuse_ino_t ino, size_t size,
 static void tracing_readdirplus(fuse_req_t req, fuse_ino_t ino, size_t size,
 	off_t offset, fuse_file_info *fi) {
 	auto span = getSpan(LIB_NAME, "Read Directory Plus");
-	tracing_next->readdir(req, ino, size, offset, fi);
+	setAttribute(span, req);
+	setAttribute(span, ino);
+	tracing_next->readdirplus(req, ino, size, offset, fi);
 	span->End();	
 }
 
 static void tracing_releasedir(fuse_req_t req, fuse_ino_t ino, fuse_file_info *fi) {
 	auto span = getSpan(LIB_NAME, "Release Directory");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->releasedir(req, ino, fi);
 	span->End();	
 }
@@ -170,6 +204,8 @@ static void tracing_releasedir(fuse_req_t req, fuse_ino_t ino, fuse_file_info *f
 static void tracing_fsyncdir(fuse_req_t req, fuse_ino_t ino, int datasync,
 	fuse_file_info *fi) {
 	auto span = getSpan(LIB_NAME, "Fsync Directory");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->fsyncdir(req, ino, datasync, fi);
 	span->End();	
 }
@@ -177,24 +213,32 @@ static void tracing_fsyncdir(fuse_req_t req, fuse_ino_t ino, int datasync,
 static void tracing_create(fuse_req_t req, fuse_ino_t parent, const char *name,
 	mode_t mode, fuse_file_info *fi) {
 	auto span = getSpan(LIB_NAME, "Create");
+	setAttribute(span, req);
+	span->SetAttribute("Name", name);
 	tracing_next->create(req, parent, name, mode, fi);
 	span->End();	
 }
 
 static void tracing_open(fuse_req_t req, fuse_ino_t ino, fuse_file_info *fi) {
 	auto span = getSpan(LIB_NAME, "Open");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->open(req, ino, fi);
 	span->End();
 }
 
 static void tracing_release(fuse_req_t req, fuse_ino_t ino, fuse_file_info *fi) {
 	auto span = getSpan(LIB_NAME, "Release");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->release(req, ino, fi);
 	span->End();
 }
 
 static void tracing_flush(fuse_req_t req, fuse_ino_t ino, fuse_file_info *fi) {
 	auto span = getSpan(LIB_NAME, "Flush");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->flush(req, ino, fi);
 	span->End();
 }
@@ -202,6 +246,8 @@ static void tracing_flush(fuse_req_t req, fuse_ino_t ino, fuse_file_info *fi) {
 static void tracing_fsync(fuse_req_t req, fuse_ino_t ino, int datasync,
 	fuse_file_info *fi) {
 	auto span = getSpan(LIB_NAME, "Fsync");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->fsync(req, ino, datasync, fi);
 	span->End();
 }
@@ -209,12 +255,12 @@ static void tracing_fsync(fuse_req_t req, fuse_ino_t ino, int datasync,
 static void tracing_read(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off,
 	struct fuse_file_info *fi) 
 {
-	auto span = getSpan(LIB_NAME, "Read");
-	span->SetAttribute("Size", size);
-	span->SetAttribute("Offset", off);
+	auto span = getSpan(LIB_NAME, "Read");	
 	setAttribute(span, req);
 	setAttribute(span, ino);
-	
+	span->SetAttribute("Size", size);
+	span->SetAttribute("Offset", off);
+
 	tracing_next->read(req, ino, size, off, fi);
 
 	span->End();
@@ -223,12 +269,16 @@ static void tracing_read(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off,
 static void tracing_write_buf(fuse_req_t req, fuse_ino_t ino, fuse_bufvec *in_buf,
                           off_t off, fuse_file_info *fi) {
 	auto span = getSpan(LIB_NAME, "Write Buf");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->write_buf(req, ino, in_buf, off, fi);
 	span->End();
 }
 
 static void tracing_statfs(fuse_req_t req, fuse_ino_t ino) {
-	auto span = getSpan(LIB_NAME, "StatFS");
+	auto span = getSpan(LIB_NAME, "Stat FS");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->statfs(req, ino);
 	span->End();
 }
@@ -237,6 +287,8 @@ static void tracing_statfs(fuse_req_t req, fuse_ino_t ino) {
 static void tracing_fallocate(fuse_req_t req, fuse_ino_t ino, int mode,
                           off_t offset, off_t length, fuse_file_info *fi) {
 	auto span = getSpan(LIB_NAME, "Fallocate");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->fallocate(req, ino, mode, offset, length, fi);
 	span->End();
 }
@@ -245,6 +297,8 @@ static void tracing_fallocate(fuse_req_t req, fuse_ino_t ino, int mode,
 static void tracing_flock(fuse_req_t req, fuse_ino_t ino, fuse_file_info *fi,
 	int op) {
 	auto span = getSpan(LIB_NAME, "Flock");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->flock(req, ino, fi, op);
 	span->End();
 }
@@ -252,26 +306,37 @@ static void tracing_flock(fuse_req_t req, fuse_ino_t ino, fuse_file_info *fi,
 #ifdef HAVE_SETXATTR
 static void tracing_setxattr(fuse_req_t req, fuse_ino_t ino, const char *name,
 	const char *value, size_t size, int flags) {
-	auto span = getSpan(LIB_NAME, "Set Xattr");
+	auto span = getSpan(LIB_NAME, "Set Extended Attribute");
+	setAttribute(span, req);
+	setAttribute(span, ino);
+	span->setAttribute("Name", name);
 	tracing_next->setxattr(req, ino, name, value, size, flags);
 	span->End();
 }
 
 static void tracing_getxattr(fuse_req_t req, fuse_ino_t ino, const char *name,
 	size_t size) {
-	auto span = getSpan(LIB_NAME, "Get Xattr");
+	auto span = getSpan(LIB_NAME, "Get Extended Attribute");
+	setAttribute(span, req);
+	setAttribute(span, ino);
+	span->setAttribute("Name", name);
 	tracing_next->getxattr(req, ino, name, size);
 	span->End();
 }
 
 static void tracing_listxattr(fuse_req_t req, fuse_ino_t ino, size_t size) {
-	auto span = getSpan(LIB_NAME, "List Xattr");
+	auto span = getSpan(LIB_NAME, "List Extended Attribute");
+	setAttribute(span, req);
+	setAttribute(span, ino);
 	tracing_next->listxattr(req, ino, size);
 	span->End();
 }
 
 static void tracing_removexattr(fuse_req_t req, fuse_ino_t ino, const char *name) {
-	auto span = getSpan(LIB_NAME, "Remove Xattr");
+	auto span = getSpan(LIB_NAME, "Remove Extended Attribute");
+	setAttribute(span, req);
+	setAttribute(span, ino);
+	span->setAttribute("Name", name);
 	tracing_next->removexattr(req, ino, name);
 	span->End();
 }
